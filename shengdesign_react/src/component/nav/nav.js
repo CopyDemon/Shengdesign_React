ad9c51b6106692005ed1f6934e87c44b830e4906
Sheng <kingdesign2012@163.com>
2020-02-25 22:58:41 -0800

    add bootstrap

--- shengdesign_react/src/component/nav/nav.js
+++ shengdesign_react/src/component/nav/nav.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+//import script
+import classSelector from './nav.css';
 
 //import data
-import nav_db from "./nav_db";
+// import nav_db from "./nav_db";
 
-const Nav = props =>{
+class Nav extends Component{
+    state = {
+        nav_data : { Home : "/", Works : "/works", About :"About" }
+    }
 
-    const Nav_db = nav_db;
-    console.log(Nav_db);
-    return (
-        <nav>
-            <ul>
-                
-            </ul>
-        </nav>
-    )
+    render(){
+        const nav_links_data = this.state.nav_data;
+        const nav_item = Object.keys(nav_links_data);
+        const nav_li = nav_item.map((item) => {
+            return(
+                <li>
+                    <a href={nav_links_data[item]}>{item}</a>
+                </li>
+            )
+        })
+        return(
+            <nav>
+                {nav_li}
+            </nav>
+        )
+    }
 }
-
 export default Nav;
