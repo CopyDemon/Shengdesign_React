910cad6feab29abd4cb25b2872be434798a9fa52
Sheng <kingdesign2012@163.com>
2020-03-03 18:30:45 -0800

    working on skill section

--- shengdesign_react/src/pages/home/home.js
+++ shengdesign_react/src/pages/home/home.js
@@ -4,20 +4,17 @@ import React, { Component } from 'react';
 //import script
 import HOC from '../../HOC/HOC';
 import SiteBanner from '../../component/Site_banner/Site_banner';
+import Skills from './skills/skills';
 
 //import data
 // import nav_db from "./nav_db";
 
 class Home extends Component{
-    state = {
-        nav_data : { Home : "/", Works : "/works", About :"About" }
-    }
-
     render(){
-        
         return(
             <HOC>
                 <SiteBanner />
+                <Skills />
             </HOC>
 
         )
---
+++ shengdesign_react/src/pages/home/skills/skills.js
@@ -0,0 +1,32 @@
+import React, { useState }from 'react';
+import {Container , Row} from 'react-bootstrap';
+
+//import scripts
+import mainClassSelector from '../../../main.module.css';
+import classSelector from './skills.module.css';
+const Skills = ( props ) =>{
+    const [skills, setSkills] = useState({
+        main_skills:['Frontend', 'Backend']
+    });
+
+    const main_skills = skills.main_skills;
+    const skills_tab = main_skills.map(item => {
+        return <span className = {`${classSelector.home_skills_tab}`}>{item}</span>
+    })
+
+    return(
+        <Container>
+            <Row>
+                <div className = {`${mainClassSelector.justify_content_center} col-lg-12 col-md-12 col-sm-12`}>
+                    <p className = {`${mainClassSelector.section_title}`}>Skills</p>
+                </div>
+                <div className = {`${mainClassSelector.justify_content_center} ${classSelector.skills_tab_container} col-lg-12 col-md-12 col-sm-12`}>
+                    {skills_tab}
+                </div>
+                
+            </Row>
+        </Container>
+    )
+}
+
+export default Skills;
