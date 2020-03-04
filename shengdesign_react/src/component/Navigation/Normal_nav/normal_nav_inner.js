724f8ba7181ebea0b662d38bcbc7cc01dc0ab298
Sheng <kingdesign2012@163.com>
2020-03-03 17:48:39 -0800

    add nav active effect

--- shengdesign_react/src/component/Navigation/Normal_nav/normal_nav_inner.js
+++ shengdesign_react/src/component/Navigation/Normal_nav/normal_nav_inner.js
@@ -5,25 +5,34 @@ import classSelector from './normal_nav.module.css';
 
 
 const Nav_container = props =>{
-        
         const nav_item = Object.keys(props.nav_data);
-
         const nav_li = nav_item.map((item) => {
             return(
-                <li className={classSelector.nav_link} key={item} id={item}>
+                <li className={`${classSelector.nav_link}`} key={item} id={`${item}_nav_item`}>
                     <a href={props.nav_data[item]}>{item}</a>
                 </li>
             )
         });
-        //const window_location = window.location.href;
-        // const current_page_name = nav_item.forEach((item)=>{
-        //     if(window_location.includes(item)){
-        //         let current_nav_item = document.getElementById(item);
-        //         current_page_name.setAttribute('class', "low")
-        //     }
-        // });
-        
 
+        
+        //set active link with under line className
+        useEffect(()=>{
+            const window_location = window.location.href;
+            const nav_list_content = Object.keys(props.nav_data);
+            const home_url = 'http://localhost:3000/';
+
+            nav_list_content.forEach((item)=>{
+                if(window_location.includes(item)){
+                    let current_nav_item = document.getElementById(`${item}_nav_item`);
+                    current_nav_item.className += ` ${classSelector.actived_nav_item}`;
+                }
+            })
+            
+            if(window_location === home_url){
+                let current_nav_item = document.getElementById("Home_nav_item");
+                current_nav_item.className += ` ${classSelector.actived_nav_item}`;
+            }
+        });
 
     return(
         <HOC>
