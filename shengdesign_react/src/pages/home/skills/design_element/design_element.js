94edee729aea3a829da84f07b731e62de7e2d03c
Sheng <kingdesign2012@163.com>
2020-03-04 22:13:46 -0800

    skill section

--- shengdesign_react/src/pages/home/skills/design_element/design_element.js
+++ shengdesign_react/src/pages/home/skills/design_element/design_element.js
@@ -7,7 +7,9 @@ import classSelector from './design_element.module.css';
 const Skills_section_design_element = () =>{
     return(
         <div className={`${classSelector.skills_desgin_element_container} col-lg-6 col-md-6 col-sm-12`}>
-            <div className={`${classSelector.skills_design_element_square}`}></div>
+            <div className={`${classSelector.skills_design_element_square}`}>
+                <div className={`${classSelector.skills_design_element_square_inner}`}></div>
+            </div>
             <div className={`${classSelector.skills_design_element_circle}`}>
                 <div className={`${classSelector.skills_design_element_line}`}></div>
             </div>
