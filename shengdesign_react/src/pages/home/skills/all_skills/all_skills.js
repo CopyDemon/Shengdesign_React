e6407e81ff589442da9e9ecda260028d39e4b6d9
Sheng <kingdesign2012@163.com>
2020-04-28 19:38:34 -0700

    working on all skills

--- shengdesign_react/src/pages/home/skills/all_skills/all_skills.js
+++ shengdesign_react/src/pages/home/skills/all_skills/all_skills.js
@@ -4,21 +4,23 @@ import React, {useState} from 'react';
 import classSelector from './all_skills.module.css';
 
 const AllSkills = (props) =>{
-    const[allSkillsState] = useState([
+    const[all_skills_state] = useState(
         {
-            baisc_title : 'All Skills',
+            title : 'I know',
             frontend_title: 'Frontend Skills',
             backend_title: 'Background Skills'
         }
-    ])
+    );
+
     return(
-        <div className={`${classSelector.all_skills_main_container}`}>
+        <div className={`${classSelector.all_skills_main_container} row col-lg-12 col-md-12 col-sm-12 px-0 mx-0`}>
             {/*title*/}
-            <p className={`${classSelector.all_skills_title}`}>All Skills</p>
+            <div className={`${classSelector.all_skills_bg}`}></div>
+            <p className={`${classSelector.all_skills_title}`}>{all_skills_state.title}</p>
 
-            <div className={`${classSelector.all_skills_content_container}`}>
+            {/* <div className={`${classSelector.all_skills_skills_container} col-lg-10 col-md-10 col-sm-12`}>
 
-            </div>
+            </div> */}
         </div>
     )
 }
