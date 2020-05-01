9caea9c05e018e50de2b727e49d37be774e62546
Sheng <kingdesign2012@163.com>
2020-05-01 12:50:31 -0700

    skills icon

--- shengdesign_react/src/pages/home/skills/all_skills/skills_content/skills_content.js
+++ shengdesign_react/src/pages/home/skills/all_skills/skills_content/skills_content.js
@@ -8,33 +8,59 @@ import classSelector from './skills_content.module.css';
 const SkillsContent = () =>{
     const [skills_content] = useState({
         html : {
-            icon:<i class="fab fa-html5"></i>
+            icon:'<i class="fab fa-html5"></i>',
+            name:"HTML"
         },
         css:{
-            icon:<i class="fab fa-css3-alt"></i>
+            icon:'<i class="fab fa-css3-alt"></i>',
+            name:"CSS"
         },
         js:{
-            icon:<i class="fab fa-js"></i>
+            icon:'<i class="fab fa-js"></i>',
+            name:"JavaScript"
         },
         node:{
-            icon:<i class="fab fa-node"></i>
+            icon:'<i class="fab fa-node"></i>',
+            name:"NodeJS"
         },
         react:{
-            icon:<i class="fab fa-react"></i>
+            icon:'<i class="fab fa-react"></i>',
+            name:"ReactJS"
         },
         vue:{
-            icon:<i class="fab fa-vuejs"></i>
+            icon:'<i class="fab fa-vuejs"></i>',
+            name:"VueJS"
         },
         angular:{
-            icon:<i class="fab fa-angular"></i>
+            icon:'<i class="fab fa-angular"></i>',
+            name:"Angular7,8"
+        },
+        mongodb:{
+            icon:'<i class="fab fa-envira"></i>',
+            name:"MongoDB"
+        },
+        sql:{
+            icon:'<i class="fas fa-database"></i>',
+            name:"SQL"
         },
         php:{
-            icon:<i class="fab fa-php"></i>
+            icon:'<i class="fab fa-php"></i>',
+            name:"PHP"
         }
-    })
+    });
+
+    // function
+    //map db and create icons
+    const skills_obj_keys = Object.keys(skills_content);
+    const a = skills_obj_keys.forEach(el=>{
+            console.log(skills_content.el)
+        })
+
+    console.log(skills_content.html)
+
     return(
         <div className={`${classSelector.skills_content_container}`}>
-
+            
         </div>
     )
 }
