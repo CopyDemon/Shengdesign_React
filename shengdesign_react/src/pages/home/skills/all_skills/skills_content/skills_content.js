24b27aeca1366b4b7d5809e23c4b74c8f99706af
Sheng <kingdesign2012@163.com>
2020-05-01 14:47:39 -0700

    working on skill card

--- shengdesign_react/src/pages/home/skills/all_skills/skills_content/skills_content.js
+++ shengdesign_react/src/pages/home/skills/all_skills/skills_content/skills_content.js
@@ -8,59 +8,64 @@ import classSelector from './skills_content.module.css';
 const SkillsContent = () =>{
     const [skills_content] = useState({
         html : {
-            icon:'<i class="fab fa-html5"></i>',
+            icon:"fa-html5",
             name:"HTML"
         },
         css:{
-            icon:'<i class="fab fa-css3-alt"></i>',
+            icon:"fa-css3-alt",
             name:"CSS"
         },
         js:{
-            icon:'<i class="fab fa-js"></i>',
+            icon:"fa-js",
             name:"JavaScript"
         },
         node:{
-            icon:'<i class="fab fa-node"></i>',
+            icon:"fa-node",
             name:"NodeJS"
         },
         react:{
-            icon:'<i class="fab fa-react"></i>',
+            icon:"fa-react",
             name:"ReactJS"
         },
         vue:{
-            icon:'<i class="fab fa-vuejs"></i>',
+            icon:"fa-vuejs",
             name:"VueJS"
         },
         angular:{
-            icon:'<i class="fab fa-angular"></i>',
+            icon:"fa-angular",
             name:"Angular7,8"
         },
         mongodb:{
-            icon:'<i class="fab fa-envira"></i>',
+            icon:"fa-envira",
             name:"MongoDB"
         },
         sql:{
-            icon:'<i class="fas fa-database"></i>',
+            icon:"fa-database",
             name:"SQL"
         },
         php:{
-            icon:'<i class="fab fa-php"></i>',
+            icon:"fa-php",
             name:"PHP"
         }
     });
 
     // function
     //map db and create icons
-    const skills_obj_keys = Object.keys(skills_content);
-    const a = skills_obj_keys.forEach(el=>{
-            console.log(skills_content.el)
+    const skills_cards = Object.keys(skills_content).map(el=>{
+        return[...Array(skills_content[el])].map((el)=>{
+            return(
+                <div className={`${classSelector.skill_card}`}>
+                    <i className={`fab ${el.icon}`}></i>
+                </div>
+            )
         })
-
-    console.log(skills_content.html)
+    });
 
     return(
         <div className={`${classSelector.skills_content_container}`}>
-            
+            {skills_cards}
+            <i className="fab fa-html5"></i>
+            <i className="fab fa-html5"></i>
         </div>
     )
 }
