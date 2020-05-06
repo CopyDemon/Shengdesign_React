833932a62e86618e7763100362e9a78a9c04c187
Sheng <kingdesign2012@163.com>
2020-05-06 13:45:29 -0700

    testing skill moc scroll show

--- shengdesign_react/src/pages/home/skills/design_element/design_element.js
+++ shengdesign_react/src/pages/home/skills/design_element/design_element.js
@@ -5,18 +5,20 @@ import classSelector from './design_element.module.css';
 
     
 const Skills_section_design_element = () =>{
-    const skills_design_element_mock = <div className={`${classSelector.skills_design_element_mock_container}`}>
-                    <div className={`${classSelector.skills_design_element_mock_browser_btn_inner_container}`}>
-                        <span className={`${classSelector.skills_design_element_mock_browser_btn}`}></span>
-                        <span className={`${classSelector.skills_design_element_mock_browser_btn}`}></span>
-                        <span className={`${classSelector.skills_design_element_mock_browser_btn}`}></span>
-                    </div>
-                </div>
+    
+    let skills_design_element_mock = "";
     useEffect(()=>{
         const skills_desgin_element_container = document.getElementById('skills_desgin_element_container');
-        console.log(skills_desgin_element_container.getClientRects() + "??")
-        setTimeout(()=>{
-        },2000)
+        const skills_design_element_content_container_top = skills_desgin_element_container.getBoundingClientRect().top;
+        window.onscroll = () => {
+            if(window.scrollY + window.innerHeight >  skills_design_element_content_container_top + skills_desgin_element_container.clientHeight /2){
+                skills_design_element_mock = <div className={`${classSelector.skills_design_element_mock_container}`}>
+                                                <div className={`test`}>
+                                                </div>
+                                            </div>
+                console.log(skills_design_element_mock)
+            }
+        }
     })
     return(
         <div className={`${classSelector.skills_desgin_element_container} col-lg-6 col-md-12 col-sm-12 px-0`} id="skills_desgin_element_container">
