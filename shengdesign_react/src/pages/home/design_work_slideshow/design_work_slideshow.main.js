6df738b859cfc3c49f055614f2048cfbaed4b37b
Sheng <kingdesign2012@163.com>
2020-05-06 22:29:32 -0700

    working on design work slideshow area

--- shengdesign_react/src/pages/home/design_work_slideshow/design_work_slideshow.main.js
+++ shengdesign_react/src/pages/home/design_work_slideshow/design_work_slideshow.main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 //import css
-import classSelector from './design_work_slideshow.main.module.css';
+import classSelector from './design_work_slideshow.module.css';
 
 const DesignWorkSlideShow = () =>{
     return (
-        <section className={`${classSelector.design_work_slideshow_main_container}container-fluid`}>
-            asd
+        <section className={`${classSelector.design_work_slideshow_main_container} container-fluid px-0 mx-0`}>
+            <p className={`${classSelector.design_work_slideshow_main_title}`}>Work Showcase Overview</p>
+            <div className={`row col-lg-12 col-md-12 col-sm-12`}>
+                
+            </div>
         </section>
     )
 }
